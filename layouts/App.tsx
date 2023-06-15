7576698e6f75040ae7cb404d19f0009632a6d0fa
joeylee <leeyw910205@gmail.com>
2023-06-15 23:29:49 +0900

    init: frontend env 세팅

---
+++ layouts/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import loadable from '@loadable/component';
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="login" />
+      <Route path="/login" component={LogIn} />
+      <Route path={'/signup'} component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
