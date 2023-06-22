2836b1404f0bed31084b2e30e6ba69d38f7a3090
joey.kinder <joey.kinder@shinhan.com>
2023-06-22 13:33:06 +0900

    feat: dm 페이지 생성

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import {Container} from "@pages/Channel/styles";
+import gravatar from "gravatar";
+import { Header } from "@pages/DirectMessage/styles";
+import useSWR from "swr";
+import {IUser} from "@typings/db";
+import fetcher from "@utils/fethcer";
+import {useParams} from "react-router";
+import ChatBox from "@components/ChatBox";
 
 const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR<IUser>('/api/users', fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
   return (
-      <div>dm</div>
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro'})} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      {/*<ChatList />*/}
+      <ChatBox chat="hello" />
+    </Container>
   );
 };
 
